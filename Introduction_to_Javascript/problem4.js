7229bf002fc3419db245208bbed00269c394e509
Biswajit Roy <broy16431@gmail.com>
2023-01-30 22:17:04 +0530

    Variables replit files

---
+++ Introduction_to_Javascript/problem4.js
@@ -0,0 +1,40 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+
+let a=`******************************************************`
+let b=`*____________________________________________________*`;
+let rep= "*               🅁 🄴 🄿 🄾 🅁 🅃  🄲 🄰 🅁 🄳              *";
+console.log(a+"\n"+rep+"\n"+b);
+let d= `*                                                    *`;
+let school="*         🅼 🅰 🆂 🅰 🅸   🆂 🅲 🅷 🅾 🅾 🅻          *";
+let c=`*                                                    *`;
+console.log(d+"\n"+school+"\n"+c);
+let name="*   Biswajit Roy                                     *";
+
+console.log(name);
+let grade=8;
+console.log("*   Grade   :", grade,`                                     *`);
+let section="*   Section : fw26                                   *";
+console.log(section);
+let roll=234;
+let e=`*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*`;
+console.log( "*   Roll No.:",roll,`                                   *`+"\n"+e);
+let score= "*  Subject     :Full Marks  : Marks Obtained         *";
+console.log(score+"\n"+`*                                                    *`);
+let sub1="     :10          :  7.5";
+console.log("*  Coding",sub1,`                  *`+"\n"+`*                                                    *`);
+let sub2= "        :10          :  6.8";
+console.log("*  DSA",sub2,`                  *`+"\n"+`*                                                    *`);
+let sub3= "       :10          :  6.0";
+console.log("*  CSBT",sub3,`                  *`);
+let f= `*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*`
+let res=":30          :  21.3";
+console.log(f+"\n"+"*  Total Marks",res,"                 *");
+let g=`*                                                    *`
+let remarks="*  Remarks     : Passed                              *";
+console.log(g+"\n"+remarks);
+let h=`******************************************************`
+console.log(h);
